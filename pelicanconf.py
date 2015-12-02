86af1673b32af8522ef530f64ad1fb38d638eeeb
Elyézer Mendes Rezende <elyezermr@gmail.com>
2015-12-02 19:40:39 -0200

    Update config and add secure token

--- pelicanconf.py
+++ pelicanconf.py
@@ -20,14 +20,11 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (('Castálio Podcast', 'http://castalio.info/'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('Twitter', 'https://twitter.com/elyezer'),
+          ('Github', 'https://github.com/elyezer'),)
 
 DEFAULT_PAGINATION = 10
 
