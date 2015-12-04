a67a96c61aacdb12521cf879368c387c2d887f03
Elyézer Mendes Rezende <elyezermr@gmail.com>
2015-12-04 14:49:01 -0200

    Add CNAME file

--- pelicanconf.py
+++ pelicanconf.py
@@ -28,5 +28,9 @@ SOCIAL = (('Twitter', 'https://twitter.com/elyezer'),
 
 DEFAULT_PAGINATION = 10
 
+STATIC_PATHS = ['images', 'extra/CNAME']
+EXTRA_PATH_METADATA = {'extra/CNAME': {'path': 'CNAME'}}
+
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
