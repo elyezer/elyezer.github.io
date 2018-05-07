c439490749dffc7d4e206cf9a0b7c7561221dc13
Elyézer Rezende <elyezermr@gmail.com>
2018-05-07 19:33:30 -0300

    Switch to HTTPS

--- publishconf.py
+++ publishconf.py
@@ -10,7 +10,7 @@ import sys
 sys.path.append(os.curdir)
 from pelicanconf import *
 
-SITEURL = 'http://elyezer.com'
+SITEURL = 'https://elyezer.com'
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/%s.atom.xml'
